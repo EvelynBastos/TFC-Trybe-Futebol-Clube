4c7c44f9d423a94ddbef9692407a73b8d8acdcdf
EvelynBastos <lynbastos@icloud.com>
2024-03-10 12:23:50 -0300

    creating endpoint /role and token validations

--- app/backend/src/middlewares/LoginValidations.ts
+++ app/backend/src/middlewares/LoginValidations.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import mapStatusHTTP from '../utils/mapStatusHTTP';
+import JWT from '../utils/JWT';
 
 export default class LoginValidations {
   static validateLogin(req: Request, res: Response, next: NextFunction): Response | void {
@@ -19,6 +20,22 @@ export default class LoginValidations {
       return res.status(mapStatusHTTP.unauthorized).json({ message: 'Invalid email or password' });
     }
 
+    next();
+  }
+
+  static async validateToken(req: Request, res: Response, next: NextFunction) {
+    const bToken = req.headers.authorization;
+
+    if (!bToken) {
+      return res.status(mapStatusHTTP.unauthorized).json({ message: 'Token not found' });
+    }
+    const token = bToken.split(' ')[1];
+    const validToken = await JWT.verify(token);
+
+    if (validToken === 'Token must be a valid token') {
+      return res.status(mapStatusHTTP.unauthorized).json({ message: validToken });
+    }
+    res.locals.payload = validToken;
     next();
   }
 }
